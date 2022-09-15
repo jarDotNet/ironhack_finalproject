0efdaa4467ce4a69850885203f4403d76182200d
José Antonio Reyes <joseantonio@yukisoftware.es>
2022-09-15 21:02:49 +0200

    Add Supabase connection + Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
 <template>
-  <div>
-    <p>Hello World!</p>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Profile v-if="store.user" />
+    <Auth v-else />
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import { store } from "./store/auth";
+import { supabase } from "./supabase";
+import Auth from "./components/Auth.vue";
+import Profile from "./components/Profile.vue";
+
+export default {
+  components: {
+    Auth,
+    Profile,
+  },
+
+  setup() {
+    store.user = supabase.auth.user();
+    supabase.auth.onAuthStateChange((_, session) => {
+      store.user = session.user;
+    });
+
+    return {
+      store,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import { createPinia } from "pinia";
 import piniaPersist from "pinia-plugin-persist";
 import router from "./router";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.js";
+
 import "./assets/main.css";
 import App from "./App.vue";
 
