934058ab33fbe7955017393c5ccb04e30c6bea7c
A. Calleja <albacalpa@gmail.com>
2022-09-22 18:23:16 +0200

    fontawesome added

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import { createPinia } from "pinia";
 import piniaPersist from "pinia-plugin-persist";
 import router from "./router";
 
-import "ress/ress.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.js";
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, far)
 
 import "./assets/main.css";
 import App from "./App.vue";
@@ -14,7 +20,8 @@ import App from "./App.vue";
 const pinia = createPinia();
 pinia.use(piniaPersist);
 
-const app = createApp(App);
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon);
 app.use(pinia);
 app.use(router);
 app.mount("#app");
+
