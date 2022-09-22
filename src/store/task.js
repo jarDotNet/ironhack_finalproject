1b4647f21d80cbc8b33bb973d27bb767cc9a91d8
José Antonio Reyes <joseantonio@yukisoftware.es>
2022-09-22 20:46:44 +0200

    Change 'comments' field to 'description'

--- src/store/task.js
+++ src/store/task.js
@@ -36,14 +36,14 @@ export default defineStore("tasks", {
         this.tasks.push(taskCreated[0]);
       }
     },
-    async updateTask(taskId, title, state, comments) {
+    async updateTask(taskId, title, state, desc) {
       alertStore.clear();
       const { data, error } = await supabase
         .from("tasks")
         .update({
           title: title,
           current_state: state,
-          comments: comments,
+          description: desc,
         })
         .match({ id: taskId });
       if (error) {
