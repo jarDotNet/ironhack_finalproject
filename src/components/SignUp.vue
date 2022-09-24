719343edeb203851ac9da1097df3ec5b31897352
A. Calleja <albacalpa@gmail.com>
2022-09-24 11:20:31 +0200

    signup form css

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <h2>Sign up for an account</h2>
-    <form @submit.prevent="handleSignup">
+    <h2 class="mb-5">Sign Up</h2>
+    <form class="w-75" @submit.prevent="handleSignup">
       <div>
         <input id="email" type="email" v-model="email" placeholder="E-mail" />
       </div>
       <div>
         <input id="password" type="password" v-model="password" placeholder="Password" />
       </div>
-      <div>
-        <button type="submit">Sign up</button>
+      
+      <div class="d-grid">
+        <button type="submit" class="btn btn-custom btn-lg btn-block mb-5">Sign up</button>
       </div>
+
     </form>
-  </div>
 </template>
 
 <script>
@@ -50,3 +50,8 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+
+</style>
--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -2,15 +2,22 @@
 <div class="main-container">
 
   <div class="main container-sm"> 
-    <sign-up v-if="isSignUp" />
+    <div class="form-signup">
+      <sign-up v-if="isSignUp" />
     <sign-in v-else />
-    <button @click="isSignUp = !isSignUp">
+
+    
+    <div class="footer-signup pt-2">
+    <button class="btn btn-link text-capitalize text-dark text-form mb-2" @click="isSignUp = !isSignUp">
       {{
         isSignUp
           ? "Already have an account? Sign In"
           : "Don't have an account yet? Sign Up"
       }}
     </button>
+  </div>
+  
+    </div>
   </div>
 
 </div>
@@ -41,10 +48,46 @@ export default {
   
   .main{
 	width: 345px;
-	height: 500px;
+	height: 400px;
 	border-radius: 13px;
 	box-shadow: 5px 20px 50px #000;
   justify-content: center;
   align-self: center;
+  padding: 0;
+  position: relative;
 }
+
+.form-signup{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.footer-signup{
+    background: #eee;
+    border-radius: 60% / 10%;
+    background: #eee;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding-top: 5px;
+    height: 40px;
+    transition: height 2.6s;
+}
+
+.footer-signup:hover{
+  height: 70px;
+}
+
+.text-form{
+  text-decoration: none;
+}
+
+
+
+
 </style>
