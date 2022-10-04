89dc77ec03294dfaf0b9803edddfd50b855c1bf6
A. Calleja <albacalpa@gmail.com>
2022-10-04 19:31:38 +0200

    profile + logout added to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,63 @@
 <template>
-  <nav v-if="visible" class="navbar navbar-expand-lg navbar-light px-5 py-3">
-    <div class="mx-3">
-      <a class="navbar-brand font-weight-bold" href="/">TrackLab</a>
-    </div>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse mx-5" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <router-link to="/" class="nav-link text-white">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/dashboard" class="nav-link text-white"
-            >Dashboard</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link text-white"
-            >Profile</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link to="/404" class="nav-link text-white">404</router-link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+   <nav
+      v-if="visible"
+      class="navbar navbar-expand-sm navbar-expand-lg navbar-light px-5 py-3"
+   >
+      <div class="container-fluid">
+         <a class="navbar-brand font-weight-bold" href="/">TrackLab</a>
+         <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapsibleNavbar"
+         >
+            <font-awesome-icon icon="fa-solid fa-bars" />
+         </button>
+         <div class="collapse navbar-collapse justify-content-between" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+               <li class="nav-item active">
+                  <router-link to="/" class="nav-link text-white"
+                     >Home</router-link
+                  >
+               </li>
+               <li class="nav-item">
+                  <router-link to="/dashboard" class="nav-link text-white"
+                     >Dashboard</router-link
+                  >
+               </li>
+               <li class="nav-item">
+                  <router-link to="/profile" class="nav-link text-white"
+                     >Profile</router-link
+                  >
+               </li>
+               <li class="nav-item">
+                  <router-link to="/404" class="nav-link text-white"
+                     >404</router-link
+                  >
+               </li>
+            </ul>
+
+            <div class="navbar-nav ml-auto gap-3 flex-row justify-content-end">
+              <a href="/profile">
+            <img
+               src="../assets/defaultAvatar.png"
+               alt="Profile photo"
+               class="rounded-circle"
+               style="width: 30px; height: 30px; object-fit: contain"
+            /></a>
+
+            <button
+               class="btn btn-custom btn-sm btn-block btn-profile my-0"
+               @click="signOut"
+               :disabled="loading"
+               title="Log Out"
+            >
+               <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" />
+            </button>
+         </div>
+         </div>
+      </div>
+   </nav>
 </template>
 
 <!-- <script>
@@ -47,37 +70,40 @@ export default {
 </script> -->
 <script setup>
 const props = defineProps({
-  visible: Boolean,
+   visible: Boolean,
 });
 </script>
 
 <style scoped>
+:deep(path) {
+   color: #fff;
+}
 .navbar-brand {
-  font-weight: bold;
-  color: #fff;
+   font-weight: bold;
+   color: #fff;
 }
 
 nav li {
-  margin-right: 7px;
-  border-radius: 15px;
-  text-transform: lowercase;
+   margin-right: 7px;
+   border-radius: 15px;
+   text-transform: lowercase;
 }
 
 .nav-link {
-  padding: 2px var(--bs-nav-link-padding-x);
+   padding: 2px var(--bs-nav-link-padding-x);
 }
 
 nav li:hover,
 nav li.router-link-active,
 nav li.router-link-exact-active {
-  background-color: #6d0abe;
-  border-radius: 15px 5px;
-  cursor: pointer;
-  transition: all 0.5s linear;
+   background-color: #6d0abe;
+   border-radius: 15px 5px;
+   cursor: pointer;
+   transition: all 0.5s linear;
 }
 
 .router-link-active {
-  background-color: #6d0abe;
-  border-radius: 15px 5px;
+   background-color: #6d0abe;
+   border-radius: 15px 5px;
 }
 </style>
