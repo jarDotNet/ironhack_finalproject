89fab8692bf9f2820ed3b4e160507bb60248fc00
A. Calleja <albacalpa@gmail.com>
2022-10-09 18:41:33 +0200

    priority and category in same row

--- src/components/CardEdition.vue
+++ src/components/CardEdition.vue
@@ -1,98 +1,111 @@
 <template>
-  <form v-if="state.task" @submit.prevent="$emit('saveTask', state.task)">
-    <label for="task-name" class="label-text text-capitalize">Task Name:</label>
-    <div class="inputfield">
-      <input
-        type="text"
-        id="task-name"
-        name="task-name"
-        class="inputField"
-        placeholder="Task name"
-        v-model="state.task.title"
-        :maxlength="ValidationConstants.NEW_TASK_MAX_LENGTH"
-        style="color: black"
-      />
-    </div>
-    <label for="task-state" class="label-text text-capitalize"
-      >Current State:</label
-    >
-    <div class="inputfield">
-      <select
-        id="task-state"
-        name="task-state"
-        class="form-control"
-        v-model="state.task.current_state"
+   <form v-if="state.task" @submit.prevent="$emit('saveTask', state.task)">
+      <label for="task-name" class="label-text text-capitalize"
+         >Task Name:</label
       >
-        <option
-          v-for="state in TaskStateEnum"
-          :key="state"
-          :value="state"
-          style="color: black"
-        >
-          {{ capitalize(state) }}
-        </option>
-      </select>
-    </div>
-    <label for="task-priority" class="label-text text-capitalize"
-      >Priority:</label
-    >
-    <div class="inputfield">
-      <select
-        id="task-priority"
-        name="task-priority"
-        class="form-control"
-        v-model="state.task.priority"
+      <div class="inputfield">
+         <input
+            type="text"
+            id="task-name"
+            name="task-name"
+            class="inputField"
+            placeholder="Task name"
+            v-model="state.task.title"
+            :maxlength="ValidationConstants.NEW_TASK_MAX_LENGTH"
+            style="color: black"
+         />
+      </div>
+      <label for="task-state" class="label-text text-capitalize"
+         >Current State:</label
       >
-        <option
-          v-for="priority in TaskPriorityEnum"
-          :key="priority"
-          :value="priority"
-          style="color: black"
-        >
-          {{ capitalize(priority) }}
-        </option>
-      </select>
-    </div>
-    <label for="task-priority" class="label-text text-capitalize"
-      >Category:</label
-    >
-    <div class="inputfield">
-      <select
-        id="task-category"
-        name="task-category"
-        class="form-control"
-        v-model="state.task.category"
-      >
-        <option
-          v-for="category in TaskCategoryEnum"
-          :key="category.name"
-          :value="category.name"
-          style="color: black"
-        >
-          {{ capitalize(category.name) }}
-        </option>
-      </select>
-    </div>
-    <label for="task-description" class="label-text text-capitalize"
-      >Description:</label
-    >
-    <div class="inputfield">
-      <textarea
-        id="task-description"
-        name="task-description"
-        v-model="state.task.description"
-        rows="4"
-        cols="50"
-        style="color: black; width: 100%"
-      />
-    </div>
+      <div class="inputfield">
+         <select
+            id="task-state"
+            name="task-state"
+            class="form-control"
+            v-model="state.task.current_state"
+         >
+            <option
+               v-for="state in TaskStateEnum"
+               :key="state"
+               :value="state"
+               style="color: black"
+            >
+               {{ capitalize(state) }}
+            </option>
+         </select>
+      </div>
 
-    <div class="d-grid gap-2">
-      <button type="submit" class="btn btn-custom btn-lg btn-block btn-profile">
-        Update Task
-      </button>
-    </div>
-  </form>
+      <div class="row d-flex flex-row mx-0 justify-content-between">
+         <div class="col-6 px-0">
+            <label for="task-priority" class="label-text text-capitalize"
+               >Priority:</label
+            >
+            <div class="inputfield">
+               <select
+                  id="task-priority"
+                  name="task-priority"
+                  class="form-control"
+                  v-model="state.task.priority"
+               >
+                  <option
+                     v-for="priority in TaskPriorityEnum"
+                     :key="priority"
+                     :value="priority"
+                     style="color: black"
+                  >
+                     {{ capitalize(priority) }}
+                  </option>
+               </select>
+            </div>
+         </div>
+         <div class="col-6 px-0">
+            <label for="task-priority" class="label-text text-capitalize"
+               >Category:</label
+            >
+            <div class="inputfield">
+               <select
+                  id="task-category"
+                  name="task-category"
+                  class="form-control"
+                  v-model="state.task.category"
+               >
+                  <option
+                     v-for="category in TaskCategoryEnum"
+                     :key="category.name"
+                     :value="category.name"
+                     style="color: black"
+                  >
+                     {{ capitalize(category.name) }}
+                  </option>
+               </select>
+            </div>
+         </div>
+      </div>
+
+      <label for="task-description" class="label-text text-capitalize"
+         >Description:</label
+      >
+      <div class="inputfield">
+         <textarea
+            id="task-description"
+            name="task-description"
+            v-model="state.task.description"
+            rows="4"
+            cols="50"
+            style="color: black; width: 100%"
+         />
+      </div>
+
+      <div class="d-grid gap-2">
+         <button
+            type="submit"
+            class="btn btn-custom btn-lg btn-block btn-profile"
+         >
+            Update Task
+         </button>
+      </div>
+   </form>
 </template>
 
 <script>
@@ -101,36 +114,36 @@ import { TaskStateEnum, TaskPriorityEnum, TaskCategoryEnum } from "../enums/";
 import ValidationConstants from "../utils/ValidationConstants";
 
 export default defineComponent({
-  name: "CardEdition",
-  emits: ["saveTask"],
-  props: {
-    editTask: {
-      type: Object,
-      default: null,
-    },
-  },
-  created() {
-    this.TaskStateEnum = TaskStateEnum;
-    this.TaskPriorityEnum = TaskPriorityEnum;
-    this.TaskCategoryEnum = TaskCategoryEnum;
-    this.ValidationConstants = ValidationConstants;
-  },
-  setup(props, { emit }) {
-    const state = reactive({
-      task: null,
-    });
+   name: "CardEdition",
+   emits: ["saveTask"],
+   props: {
+      editTask: {
+         type: Object,
+         default: null,
+      },
+   },
+   created() {
+      this.TaskStateEnum = TaskStateEnum;
+      this.TaskPriorityEnum = TaskPriorityEnum;
+      this.TaskCategoryEnum = TaskCategoryEnum;
+      this.ValidationConstants = ValidationConstants;
+   },
+   setup(props, { emit }) {
+      const state = reactive({
+         task: null,
+      });
 
-    const capitalize = (text) => {
-      if (!text || text.length === 0) return "";
-      return text[0].toUpperCase() + text.slice(1).toLowerCase();
-    };
+      const capitalize = (text) => {
+         if (!text || text.length === 0) return "";
+         return text[0].toUpperCase() + text.slice(1).toLowerCase();
+      };
 
-    onUpdated(() => {
-      state.task = props.editTask;
-    });
+      onUpdated(() => {
+         state.task = props.editTask;
+      });
 
-    return { state, capitalize };
-  },
+      return { state, capitalize };
+   },
 });
 </script>
 
