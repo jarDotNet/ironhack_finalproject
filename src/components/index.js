4bbdd540aa1864ef6ffb787143829b70e5886720
José Antonio Reyes <joseantonio@yukisoftware.es>
2022-10-10 17:14:46 +0200

    Apply alert component + Clean code

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 import Alert from "./Alert.vue";
-import Auth from "./Auth.vue";
 import CardEdition from "./CardEdition.vue";
 import CardList from "./CardList.vue";
 import Navbar from "./Navbar.vue";
 import SignIn from "./SignIn.vue";
 import SignUp from "./SignUp.vue";
 
-export { Alert, Auth, CardEdition, CardList, Navbar, SignIn, SignUp };
+export { Alert, CardEdition, CardList, Navbar, SignIn, SignUp };
