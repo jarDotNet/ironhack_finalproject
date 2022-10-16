5d4c39940c0ad27684a7d2e1e4ef90202c6a3798
José Antonio Reyes <joseantonio@yukisoftware.es>
2022-10-16 21:36:00 +0200

    404 error page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="not-found-container">
-    <div class="noise"></div>
-    <div class="overlay"></div>
-    <div class="terminal">
-      <h1>Error <span class="errorcode">404</span></h1>
-      <p class="output">
+    <main class="bsod container">
+      <h1 class="neg title"><span class="bg">Error - 404</span></h1>
+      <p>
         The page you are looking for might have been removed, had its name
-        changed or is temporarily unavailable.
+        changed or is temporarily unavailable, to continue:
       </p>
-      <p class="output">
-        Please try to <a href="#" @click="$router.go(-1)">go back</a> or
-        <router-link to="/">return to the homepage</router-link>.
+      <p>
+        * Go back.<br />
+        * Return to our homepage.
       </p>
-      <p class="output">Good luck.</p>
-    </div>
+      <nav class="not-found-nav">
+        <a href="#" @click="$router.go(-1)">back</a>
+        &nbsp;|&nbsp;
+        <router-link to="/">homepage</router-link>
+      </nav>
+    </main>
   </div>
 </template>
 
@@ -24,132 +26,88 @@ const router = useRouter();
 </script>
 
 <style scoped>
-@import "https://fonts.googleapis.com/css?family=Inconsolata";
+@import "https://fonts.googleapis.com/css?family=VT323";
 
+* {
+  color: #0414a7;
+  font-family: "VT323", monospace;
+  background: #0414a7;
+}
+
+h1,
+h2,
+h3,
+h4,
+p,
+a,
+.not-found-nav,
 #not-found-container {
-  min-height: 100%;
+  color: #e0e2f4;
 }
 
+p,
 #not-found-container {
-  box-sizing: border-box;
-  height: 100%;
-  background-color: #000000;
-  background-image: radial-gradient(#11581e, #041607), url("../assets/404.gif");
-  background-repeat: no-repeat;
-  background-size: cover;
-  font-size: 1.5rem;
-  text-align: left;
+  font: normal 25px/1.5rem "VT323", monospace;
 }
-
-#not-found-container * {
-  font-family: "Inconsolata", Helvetica, sans-serif;
-  text-shadow: 0 0 1ex rgba(51, 255, 51, 1), 0 0 2px rgba(255, 255, 255, 0.8);
-}
-
-.noise {
-  pointer-events: none;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-image: url("../assets/404.gif");
-  background-repeat: no-repeat;
-  background-size: cover;
-  z-index: -1;
-  opacity: 0.02;
-}
-
-.overlay {
-  pointer-events: none;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: repeating-linear-gradient(
-    180deg,
-    rgba(0, 0, 0, 0) 0,
-    rgba(0, 0, 0, 0.3) 50%,
-    rgba(0, 0, 0, 0) 100%
-  );
-  background-size: auto 4px;
-  z-index: 1;
-  border-radius: 32px 32px 0 0;
-}
-
-.overlay::before {
-  content: "";
-  pointer-events: none;
-  position: absolute;
-  display: block;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  background-image: linear-gradient(
-    0deg,
-    transparent 0%,
-    rgba(32, 128, 32, 0.2) 2%,
-    rgba(32, 128, 32, 0.8) 3%,
-    rgba(32, 128, 32, 0.2) 3%,
-    transparent 100%
-  );
-  background-repeat: no-repeat;
-  animation: scan 7.5s linear 0s infinite;
-}
-
-@keyframes scan {
-  0% {
-    background-position: 0 -100vh;
-  }
-  35%,
-  100% {
-    background-position: 0 100vh;
-  }
-}
-
-.terminal {
-  box-sizing: inherit;
-  position: absolute;
-  height: 100%;
-  width: 1000px;
-  max-width: 100%;
-  padding: 5rem;
-  text-transform: uppercase;
-}
-
-.output {
-  color: rgba(128, 255, 128, 0.8);
-  text-shadow: 0 0 1px rgba(51, 255, 51, 0.4), 0 0 2px rgba(255, 255, 255, 0.8);
-}
-
-.output::before {
-  content: "> ";
-}
-
 h1 {
-  font-size: 3rem;
-  font-weight: 800;
-  color: rgba(128, 255, 128, 0.8);
+  font: normal 2.75rem/1.05em "VT323", monospace;
+}
+h2 {
+  font: normal 2.25rem/1.25em "VT323", monospace;
+}
+h3 {
+  font: lighter 1.5rem/1.25em "VT323", monospace;
+}
+h4 {
+  font: lighter 1.125rem/1.2222222em "VT323", monospace;
 }
 
-a {
-  color: #fff;
+#not-found-container {
+  position: absolute;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+  top: 70px;
+  bottom: 0;
+  width: 98%;
+}
+
+.container {
+  width: 90%;
+  margin: auto;
+  max-width: 640px;
+}
+
+.bsod {
+  padding-top: 10%;
+}
+
+.bsod .neg {
+  text-align: center;
+  color: #0414a7;
+}
+
+.bsod .neg .bg {
+  background: #aaa;
+  padding: 0 15px 2px 13px;
+}
+
+.bsod .title {
+  margin-bottom: 50px;
+}
+
+.bsod .not-found-nav {
+  margin-top: 35px;
+  text-align: center;
+}
+
+.bsod .not-found-nav a {
   text-decoration: none;
+  padding: 0 9px 2px 8px;
 }
 
-a:hover {
-  color: #ccc;
-}
-
-a::before {
-  content: "[";
-}
-
-a::after {
-  content: "]";
-}
-
-.errorcode {
-  color: white;
+.bsod .not-found-nav a:hover:hover,
+.bsod .not-found-nav a:focus {
+  background: #aaa;
+  color: #0414a7;
 }
 </style>
